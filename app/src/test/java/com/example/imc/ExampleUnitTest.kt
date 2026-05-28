d61408a3cdf44984b0228ec1635ec65bd948adef
IaraTassi <iaratassi@hotmail.com>
2026-05-28 13:20:02 -0300

    test(imc): add basic unit tests for calculation and classification

--- app/src/test/java/com/example/imc/ExampleUnitTest.kt
+++ app/src/test/java/com/example/imc/ExampleUnitTest.kt
@@ -9,9 +9,25 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+
+class ImcCalculatorTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun deve_calcular_imc_e_retornar_categoria_correta() {
+        val imc = IMC("Maria", 70f, 170f)
+
+        val resultado = imc.calcular()
+
+        assertEquals("Saudável", resultado)
     }
+
+
+    @Test
+    fun deve_ser_calculado_imc_corretamente() {
+        val imc = IMC("Maria", 70f, 170f)
+
+        imc.calcular()
+
+        assertEquals(24.22f, imc.imc, 0.01f)
+    }
+
 }
